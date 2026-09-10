65725ae06babb6ce111952d3fa437fd7ed145d63
patrikkytka2-PNG <patrikkytka2@gmail.com>
2026-09-10 11:12:06 +0200

    naucili sme sa pouzivat print

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
-<?php
-echo "Hello, World!";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "stránka";
+    ?>
+
+    <div>
+        <p></p>
+    </div>
+
+    <?php
+        echo "<p>tento kod je vnutrom php</p>";
+
+        print("toto je moj vypis pomocou print");
+    ?>
+
+    <?php
+
+    ?>
+</body>
+</html>
